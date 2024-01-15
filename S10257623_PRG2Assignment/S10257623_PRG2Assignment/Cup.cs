491756e3096f6981e997df64e4e1faed8618e80d
LuKaito1412 <lu176844@gmail.com>
2024-01-15 15:25:22 +0800

    File rearrange

---
+++ S10257623_PRG2Assignment/S10257623_PRG2Assignment/Cup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Cup: IceCream
+{
+    public Cup() { }
+    public Cup(string option, int scoops, List<Flavour> flavours, List<Topping> toppings) :base(option, scoops, flavours, toppings)
+    {
+
+    }
+    public override double CalculatePrice()
+    {
+        
+    }
+}
+
--- S10257623_PRG2Assignment/S10257623_PRG2Assignment/IceCream.cs
+++ S10257623_PRG2Assignment/S10257623_PRG2Assignment/IceCream.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace S10257623_PRG2Assignment
+   
+abstract class IceCream
 {
-    internal class IceCream
+    public string Option { get; set; }
+    public int Scoops { get; set; }
+    
+    public List<Flavour> Flavours { get; set; } = new List<Flavour>();
+    public List<Topping> Toppings { get; set; } = new List<Topping>();
+
+    public IceCream() { }
+    public IceCream(string option, int scoops, List<Flavour> flavours, List<Topping> toppings)
     {
+        Option = option;
+        Scoops = scoops;
+        Flavours = flavours;
+        Toppings = toppings;
+    }
+    public abstract double CalculatePrice();
+
+
+    // to string method not fully done yet
+
+    public override string ToString()
+    {
+        return $"Option: {Option} Scoops: {Scoops}";
     }
 }
+
