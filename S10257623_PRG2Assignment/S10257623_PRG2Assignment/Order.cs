60ba36b51232529594b67b185534633dc59f08b2
LuKaito1412 <lu176844@gmail.com>
2024-01-15 15:55:19 +0800

    Addition of 3 classes

---
+++ S10257623_PRG2Assignment/S10257623_PRG2Assignment/Order.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Order
+{
+    public int Id {get; set;}
+    public DateTime TimeReceived {get; set;}
+    public DateTime? TimeFulfuilled {get; set;}
+    public List<IceCream> IceCreamList {get; set;} = new List<IceCream>();
+
+    public Order() {}
+    public Order(int i, DateTime t)
+    {
+        Id = i;
+        TimeReceived = t;
+    }
+}
